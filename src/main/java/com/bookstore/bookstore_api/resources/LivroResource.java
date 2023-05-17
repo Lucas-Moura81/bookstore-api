f988c16e2b1fed7f17f330249d47cd04a9343543
Bryan <lucas.inocenciotrabalho12@gmail.com>
2023-05-17 16:48:17 -0300

    delete livros

--- src/main/java/com/bookstore/bookstore_api/resources/LivroResource.java
+++ src/main/java/com/bookstore/bookstore_api/resources/LivroResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -48,5 +49,11 @@ public class LivroResource {
 		 Livro newObj = service.create(id_cat, obj); 
 		 URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/livros/{id}").buildAndExpand(newObj.getId()).toUri();
 		 return ResponseEntity.created(uri).build();
+	}
+	
+	@DeleteMapping (value ="/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/bookstore/bookstore_api/service/LivroService.java
+++ src/main/java/com/bookstore/bookstore_api/service/LivroService.java
@@ -13,33 +13,38 @@ import com.bookstore.bookstore_api.service.exception.ObjectNotFoundException;
 @Service
 public class LivroService {
 	@Autowired
-	LivroRepository repository;  
+	LivroRepository repository;
 	@Autowired
 	CategoriaService categoriaService;
-	
+
 	public Livro findById(Integer id) {
 		Optional<Livro> obj = repository.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException
-				("Objeto não encontrado!" + id + ", tipo: " + Livro.class.getName()));
+		return obj.orElseThrow(
+				() -> new ObjectNotFoundException("Objeto não encontrado!" + id + ", tipo: " + Livro.class.getName()));
 	}
 
 	public Livro update(Integer id, Livro obj) {
-		Livro newObj = findById(id);   // validando 
+		Livro newObj = findById(id); // validando
 		updateData(newObj, obj);
 		return repository.save(newObj);
 	}
 
 	private void updateData(Livro newObj, Livro obj) {
-		newObj.setTitulo(obj.getTitulo());    
+		newObj.setTitulo(obj.getTitulo());
 		newObj.setNome_autor(obj.getNome_autor());
 		newObj.setTexto(obj.getTexto());
 	}
 
 	public Livro create(Integer id_cat, Livro obj) {
-       obj.setId(null);
-       Categoria cat = categoriaService.findById(id_cat);
-       obj.setCategoria(cat);
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		obj.setCategoria(cat);
 		return repository.save(obj);
+	}
+
+	public void delete(Integer id) {
+		Livro obj = findById(id);
+		repository.delete(obj);
 	}
 
 }
