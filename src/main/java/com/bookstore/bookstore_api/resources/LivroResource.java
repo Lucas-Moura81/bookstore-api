5faf14c658fa74fa359d9eb84cbd98909c6ac70e
Bryan <lucas.inocenciotrabalho12@gmail.com>
2023-05-17 15:23:31 -0300

    PUT E PATCH para livro

--- src/main/java/com/bookstore/bookstore_api/resources/LivroResource.java
+++ src/main/java/com/bookstore/bookstore_api/resources/LivroResource.java
@@ -3,7 +3,10 @@ package com.bookstore.bookstore_api.resources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +23,18 @@ public class LivroResource {
 	public ResponseEntity<Livro> findById(@PathVariable Integer id){
 		Livro obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PutMapping(value ="/{id}")
+	public ResponseEntity<Livro> update(@PathVariable Integer id, @RequestBody Livro obj){
+		Livro newObj = service.update(id, obj); 
+		return ResponseEntity.ok().body(newObj); 
+	}
+	
+	@PatchMapping(value ="/{id}")
+	public ResponseEntity<Livro> updatePatch(@PathVariable Integer id, @RequestBody Livro obj){
+		Livro newObj = service.update(id, obj); 
+		return ResponseEntity.ok().body(newObj); 
 	}
 	
 }
--- src/main/java/com/bookstore/bookstore_api/service/LivroService.java
+++ src/main/java/com/bookstore/bookstore_api/service/LivroService.java
@@ -20,4 +20,16 @@ public class LivroService {
 				("Objeto não encontrado!" + id + ", tipo: " + Livro.class.getName()));
 	}
 
+	public Livro update(Integer id, Livro obj) {
+		Livro newObj = findById(id);   // validando 
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+
+	private void updateData(Livro newObj, Livro obj) {
+		newObj.setTitulo(obj.getTitulo());    
+		newObj.setNome_autor(obj.getNome_autor());
+		newObj.setTexto(obj.getTexto());
+	}
+
 }
