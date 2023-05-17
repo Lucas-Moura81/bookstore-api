a1e8f2f91d604194d6e9ba62e811bc16dc824b45
Bryan <lucas.inocenciotrabalho12@gmail.com>
2023-05-17 16:11:14 -0300

    POST para livros

--- src/main/java/com/bookstore/bookstore_api/service/LivroService.java
+++ src/main/java/com/bookstore/bookstore_api/service/LivroService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bookstore.bookstore_api.domain.Categoria;
 import com.bookstore.bookstore_api.domain.Livro;
 import com.bookstore.bookstore_api.repositories.LivroRepository;
 import com.bookstore.bookstore_api.service.exception.ObjectNotFoundException;
@@ -13,6 +14,8 @@ import com.bookstore.bookstore_api.service.exception.ObjectNotFoundException;
 public class LivroService {
 	@Autowired
 	LivroRepository repository;  
+	@Autowired
+	CategoriaService categoriaService;
 	
 	public Livro findById(Integer id) {
 		Optional<Livro> obj = repository.findById(id);
@@ -30,6 +33,13 @@ public class LivroService {
 		newObj.setTitulo(obj.getTitulo());    
 		newObj.setNome_autor(obj.getNome_autor());
 		newObj.setTexto(obj.getTexto());
+	}
+
+	public Livro create(Integer id_cat, Livro obj) {
+       obj.setId(null);
+       Categoria cat = categoriaService.findById(id_cat);
+       obj.setCategoria(cat);
+		return repository.save(obj);
 	}
 
 }
